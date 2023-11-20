6f7c309d49554d093e6f36e3f67752ff32bfb44c
ritesh3105 <ritesh.d@amazatic.com>
2023-11-20 13:50:54 +0530

    Setup react app using webpack.

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const child1 = React.createElement("p", {}, "This is Child 1");
+const child2 = React.createElement("p", {}, "This is Child 2");
+
+// const div = React.createElement("div", {}, "This is Ritesh. Using ReactJS", [
+//   child1,
+//   child2,
+// ]);
+const div= <div>Hello</div>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(div);
+
+// JSX converted into React element using babel
+// Ex. <div>Sample<div>  ->  const div = React.createElement("div", {}, "Sample");
